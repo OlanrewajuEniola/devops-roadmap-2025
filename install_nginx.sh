db44b18ef3d6c72ddfb3412afb5fce5b4e8e3d2d
Olanrewaju <olaeniola16@gmail.com>
2025-07-25 13:41:39 +0000

    commiting to repo

--- install_nginx.sh
+++ install_nginx.sh
@@ -3,4 +3,4 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install nginx -y
 sudo systemctl start nginx
 sudo systemctl enable nginx
-echo "Ola is a certified DevOps Engineer and one of the best in the world"
+echo "<h1>Ola is a certified DevOps Engineer and one of the best in the world</h1>" | sudo tee /var/www/html/index.html
